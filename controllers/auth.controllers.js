9a9a39ae3fe25e7448e7dc6bcd2984e38b658520
HazratAli <hazratali3711@gmail.com>
2024-10-03 19:16:34 +0600

    debugging

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,13 +1,22 @@
 const jwt = require("jsonwebtoken");
 
 exports.googleAuth = (req, res) => {
-  const user = req.user;
-  const token = jwt.sign(
-    { userId: user._id, email: user.email },
-    process.env.JWT_SECRET,
-    { expiresIn: "5h" }
-  );
+  try {
+    if (!req.user) {
+      return res.status(401).json({ error: "Authentication failed" });
+    }
 
-  // res.status(200).json({ token });
-  res.redirect(`${process.env.ORIGIN}/dashboard`);
+    const user = req.user;
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "5h" }
+    );
+
+    // Redirecting back to the frontend with token or storing it in cookie
+    res.redirect(`${process.env.ORIGIN}/dashboard`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
