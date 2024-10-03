c33c1211cc6878c9b4cdfb5586664f04d6991204
HazratAli <hazratali3711@gmail.com>
2024-10-03 18:31:23 +0600

    fixed bug

--- middlewares/isVerified.js
+++ middlewares/isVerified.js
@@ -1,6 +1,6 @@
-import jwt from "jsonwebtoken";
+const jwt = require("jsonwebtoken");
 
-export const isVerified = (req, res, next) => {
+const isVerified = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
@@ -15,3 +15,5 @@ export const isVerified = (req, res, next) => {
     return res.status(403).json({ message: "Invalid or expired token" });
   }
 };
+
+module.exports = isVerified;
