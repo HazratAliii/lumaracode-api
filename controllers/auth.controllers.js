86226fa440a3b532aa9febb2c672a3683c82ea0f
HazratAli <hazratali3711@gmail.com>
2024-10-03 19:18:24 +0600

    debugging

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -12,9 +12,9 @@ exports.googleAuth = (req, res) => {
       process.env.JWT_SECRET,
       { expiresIn: "5h" }
     );
-
+    res.status(200).json(token);
     // Redirecting back to the frontend with token or storing it in cookie
-    res.redirect(`${process.env.ORIGIN}/dashboard`);
+    // res.redirect(`${process.env.ORIGIN}/dashboard`);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal Server Error" });
