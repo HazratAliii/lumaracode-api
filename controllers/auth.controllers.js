d10b74b93a6c840d2356c5895fce4442dc09178f
HazratAli <hazratali3711@gmail.com>
2024-10-03 19:33:01 +0600

    debugging

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,20 +1,31 @@
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const passport = require("passport");
+const User = require("../models/user.model");
+
+exports.test = (req, res) => {
+  console.log("testing ", process.env.GOOGLE_CLIENT_ID);
+};
 
 exports.googleAuth = (req, res) => {
-  try {
-    if (!req.user) {
-      return res.status(401).json({ error: "Authentication failed" });
-    }
+  console.log("INside google auth");
 
-    const user = req.user;
-    const token = jwt.sign(
-      { userId: user._id, email: user.email },
-      process.env.JWT_SECRET,
-      { expiresIn: "5h" }
-    );
-    res.redirect("http://localhost:5173");
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  const user = req.user;
+  const token = jwt.sign(
+    { userId: user._id, email: user.email },
+    process.env.JWT_SECRET,
+    { expiresIn: "5h" }
+  );
+
+  //   // Set token as cookie
+  //   res.cookie("token", token, {
+  //     httpOnly: true,
+  //     sameSite: "none",
+  //     domain: ".ewnfe.vercel.app",
+  //     secure: true,
+  //     path: "/",
+  //     maxAge: 5 * 60 * 60 * 1000,
+  //   });
+  res.status(200).json({ token });
 };
