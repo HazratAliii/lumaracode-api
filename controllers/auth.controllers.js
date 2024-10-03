20ba4979bd94fba06ac3a3fe3767889252217603
HazratAli <hazratali3711@gmail.com>
2024-10-03 20:13:45 +0600

    modified callback function

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -4,30 +4,6 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const passport = require("passport");
 const User = require("../models/user.model");
 
-
-
-// exports.googleAuth = (req, res) => {
-//   console.log("INside google auth");
-
-//   const user = req.user;
-//   const token = jwt.sign(
-//     { userId: user._id, email: user.email },
-//     process.env.JWT_SECRET,
-//     { expiresIn: "5h" }
-//   );
-
-//   //   // Set token as cookie
-//   //   res.cookie("token", token, {
-//   //     httpOnly: true,
-//   //     sameSite: "none",
-//   //     domain: ".ewnfe.vercel.app",
-//   //     secure: true,
-//   //     path: "/",
-//   //     maxAge: 5 * 60 * 60 * 1000,
-//   //   });
-//   res.status(200).json({ token });
-// };
-
 exports.googleAuth = (req, res) => {
   try {
     if (!req.user) {
@@ -44,10 +20,7 @@ exports.googleAuth = (req, res) => {
       { expiresIn: "5h" }
     );
 
-    console.log("JWT token generated:", token);
-
-    // Redirect back to the frontend
-    res.redirect(`${process.env.ORIGIN}/dashboard`);
+    res.status(200).json({ token });
   } catch (err) {
     console.error("Error in googleAuth function:", err);
     res.status(500).json({ error: "Internal Server Error" });
